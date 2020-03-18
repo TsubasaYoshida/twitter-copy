cd342db43a7669828b563b5b39a15e1e1e20d0c5
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-03-18 23:46:43 +0900

    #4 ユーザー登録完了ページを実装する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    if user_signed_in?
+      flash[:notice] = 'ログインしました。'
+      home_index_path
+    else
+      home_sign_up_done_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'home/sign_up_done'
   devise_for :users, controllers: {
       registrations: 'users/registrations'
   }
+  root to: 'home#index'
 end
