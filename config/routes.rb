3d97a63327e69b9933131544015b1291512e7fcb
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-03-19 23:15:50 +0900

    #4 ユーザー登録完了ページのルーティングを修正する

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
-  get 'home/sign_up_done'
+  get 'sign_up_done', to: 'home#sign_up_done'
   devise_for :users, controllers: {
       registrations: 'users/registrations'
   }
