f4ce07b42a829473ccf91f6a42523847bfe718f1
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-03-20 20:14:21 +0900

    #4 リファクタリング

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     if user_signed_in?
       flash[:notice] = 'ログインしました。'
-      home_index_path
+      root_path
     else
       home_sign_up_done_path
     end
