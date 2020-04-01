a61a83e22fc4efeba1ac50ce6e930df9606a8214
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-04-01 23:32:40 +0900

    #9 単一ファイルコンポーネント化に成功する

---
+++ app/javascript/packs/components/tweets.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>{{ tweet }}</div>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    data: function(){
+      return {
+        tweet: ''
+      }
+    },
+    methods: {
+      get_tweet: function(){
+        axios
+          .get('http://localhost:3000/tweets/1.json')
+          .then(response => {
+            this.tweet = response.data.body
+          })
+      }
+    },
+    mounted: function(){
+      this.get_tweet()
+    }
+  }
+</script>
--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,30 +1,9 @@
 import Vue from 'vue/dist/vue.esm.js'
-import axios from 'axios'
-
-let Tweets = {
-  template: '<div>{{ tweet }}</div>',
-  data: function(){
-    return {
-      tweet: ''
-    };
-  },
-  methods: {
-    get_tweet: function(){
-      axios
-        .get('http://localhost:3000/tweets/1.json')
-        .then(response => {
-          this.tweet = response.data.body
-        })
-    }
-  },
-  mounted: function(){
-    this.get_tweet()
-  }
-};
+import Tweets from './components/tweets'
 
 new Vue({
   el: '#app',
   components: {
     'my-tweets': Tweets
   }
-});
+})
