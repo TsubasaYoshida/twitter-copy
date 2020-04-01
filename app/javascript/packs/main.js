cda935edd3e9161b3d6e94a2c13dcc883e56561a
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-04-01 22:43:19 +0900

    #9 GETできるようになった

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,26 +1,30 @@
 import Vue from 'vue/dist/vue.esm.js'
 import axios from 'axios'
 
-Vue.component('Tweets', {
-  template: '<div>{{ tweet }}</div>'
-});
-
-new Vue({
-  el: '#app',
-  data: {
-    tweet: ''
+let Tweets = {
+  template: '<div>{{ tweet }}</div>',
+  data: function(){
+    return {
+      tweet: ''
+    };
   },
   methods: {
     get_tweet: function(){
       axios
         .get('http://localhost:3000/tweets/1.json')
         .then(response => {
-          console.log(response.data);
           this.tweet = response.data.body
         })
     }
   },
   mounted: function(){
     this.get_tweet()
+  }
+};
+
+new Vue({
+  el: '#app',
+  components: {
+    'tweets': Tweets
   }
 });
