ac407f36b3ecfd263ccf2e0b1bbaa48c997c5eb2
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-04-06 23:15:37 +0900

    #9 vuexを導入する

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue/dist/vue.esm.js'
+import Vuex from 'vuex'
 import Tweets from './components/tweets'
 import TweetPost from './components/tweets_post'
+
+Vue.use(Vuex)
 
 new Vue({
   el: '#app',
