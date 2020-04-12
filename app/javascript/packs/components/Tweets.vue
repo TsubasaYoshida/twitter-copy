6b355d3615ae6093986e42651d86dd9ceb7036c7
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-04-12 23:45:16 +0900

    #13 ツイート一覧コンポーネントをリファクタリング&スタイリングする

--- app/javascript/packs/components/Tweets.vue
+++ app/javascript/packs/components/Tweets.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
-    <div v-for="tweet in $store.state.tweets">
-      {{ tweet.body }}
-      <span v-if="tweet.user_id === $store.state.user_id" @click="deleteTweet(tweet.id)">削除</span>
+    <div v-for="tweet in $store.state.tweets" class="tweet-item">
+      <div class="tweet-item__body">{{ tweet.body }}</div>
+      <div>
+        <div v-if="deletable(tweet)" @click="deleteTweet(tweet)" class="tweet-item__delete">削除</div>
+        <div v-else class="tweet-item__delete__disabled">削除</div>
+      </div>
     </div>
   </div>
 </template>
@@ -26,12 +29,15 @@
             this.$store.commit('setUserId', response.data.id)
           })
       },
-      deleteTweet: function(id){
+      deleteTweet: function(tweet){
         axios
-          .delete('tweets/' + id)
+          .delete('tweets/' + tweet.id)
           .then(() => {
             this.$store.dispatch('fetchTweets')
           })
+      },
+      deletable: function(tweet){
+        return tweet.user_id === this.$store.state.user_id
       },
     },
     mounted: function(){
@@ -40,3 +46,38 @@
     }
   }
 </script>
+
+<style>
+  .tweet-item {
+    background-color: #FFF;
+    padding: 24px;
+    border: solid 1px #333;
+  }
+
+  .tweet-item:not(:first-child) {
+    border-top: none;
+  }
+
+  .tweet-item__body {
+    margin-bottom: 16px;
+  }
+
+  .tweet-item__delete {
+    display: inline-block;
+    cursor: pointer;
+    color: #FFF;
+    background-color: #fc5c65;
+    padding: 8px;
+    border-radius: 4px;
+  }
+
+  .tweet-item__delete__disabled {
+    display: inline-block;
+    cursor: not-allowed;
+    color: #FFF;
+    background-color: #333;
+    opacity: .5;
+    padding: 8px;
+    border-radius: 4px;
+  }
+</style>
