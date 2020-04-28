0309582f6d0b6c33bb32fea02b9bce7212b8b3eb
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-04-28 23:07:37 +0900

    #17 ハンバーガーメニューを実装する(途中)

---
+++ app/javascript/packs/components/Hamburger.vue
@@ -0,0 +1,141 @@
+<template>
+  <div>
+    <button class="button" type="button" name="button" @click="show">
+      <span class="top"></span>
+      <span class="middle"></span>
+      <span class="bottom"></span>
+    </button>
+    <transition>
+      <nav id="gnav" class="nav" v-if="gnav">
+        <div class="nav__body">
+          <ul class="nav__lst">
+            <li class="nav__item"><a>Works</a></li>
+            <li class="nav__item"><a>Blog</a></li>
+            <li class="nav__item"><a>People</a></li>
+            <li class="nav__item"><a>Recruit</a></li>
+            <li class="nav__item"><a>Company</a></li>
+            <li class="nav__item"><a>Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+    </transition>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function(){
+      return {
+        gnav: false
+      }
+    },
+    methods: {
+      show: function(e){
+        let elm = e.currentTarget
+        let className = elm.className
+        if (className.indexOf('is-open') !== -1) {
+          this.gnav = false
+          elm.className = 'button'
+        } else {
+          this.gnav = true
+          elm.className += " is-open"
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  .button {
+    display: block;
+    width: 50px;
+    height: 50px;
+    background-color: transparent;
+    border: none;
+    position: relative;
+    z-index: 100;
+    appearance: none;
+    cursor: pointer;
+    outline: none;
+  }
+
+  .button.is-open .top {
+    transform: rotate(45deg);
+  }
+
+  .button.is-open .middle {
+    opacity: 0;
+  }
+
+  .button.is-open .bottom {
+    transform: rotate(-45deg);
+  }
+
+  .button span {
+    display: block;
+    width: 20px;
+    height: 1px;
+    margin: auto;
+    background-color: #000;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    transition: 0.5s ease;
+  }
+
+  .button span.top {
+    transform: translateY(-6px);
+  }
+
+  .button span.bottom {
+    transform: translateY(6px);
+  }
+
+  .nav {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-color: #d1dde3;
+    overflow-y: scroll;
+    z-index: 99;
+  }
+
+  .nav.v-enter, .nav.v-leave-to {
+    opacity: 0;
+  }
+
+  .nav.v-enter-to, .nav.v-leave {
+    opacity: 1;
+  }
+
+  .nav.v-enter-active, .nav.v-leave-active {
+    transition: opacity 1s ease;
+  }
+
+  .nav__body {
+    padding: 40px;
+  }
+
+  .nav__lst {
+    list-style: none;
+  }
+
+  .nav__item {
+    font-size: 24px;
+    line-height: 1.5;
+    font-weight: 700;
+  }
+
+  .nav__item ~ .nav__item {
+    margin-top: 8px;
+  }
+
+  .nav__item a {
+    color: #000;
+    padding: 8px 0;
+  }
+</style>
