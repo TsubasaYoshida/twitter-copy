b539cea51a21b3618756efe231aa3ac478d66274
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-05-04 22:57:40 +0900

    #17 タイムライン用ツイート取得メソッドのテストを実装する

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -3,4 +3,14 @@ FactoryBot.define do
     association :user, factory: :user_1
     body {'エアロビクスしようぜ。'}
   end
+
+  factory :tweet_1, class: Tweet do
+    association :user, factory: :user_1
+    body {'iPhone欲しいなあ。'}
+  end
+
+  factory :tweet_2, class: Tweet do
+    association :user, factory: :user_2
+    body {'PC欲しいなあ。'}
+  end
 end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -13,4 +13,11 @@ RSpec.describe Tweet, type: :model do
     @tweet.body = ''
     expect(@tweet.valid?).to be_falsey
   end
+
+  it "フォローしている人と自分自身が投稿したツイートを取得できること" do
+    tweet_1 = create(:tweet_1)
+    tweet_2 = create(:tweet_2)
+    Follow.create(following_id: 1, followed_id: 2)
+    expect(Tweet.timeline_tweets(tweet_1.user)).to eq [tweet_2, tweet_1]
+  end
 end
